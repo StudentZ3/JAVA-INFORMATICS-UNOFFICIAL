52ef47f0408969821c2e8e3431c7bb3fcd685fe1
Zlatozar Boev <66695667+StudentZ3@users.noreply.github.com>
2021-10-14 15:40:20 +0300

    Add files via upload

---
+++ RegularPolygon.java
@@ -0,0 +1,19 @@
+package objectsAndClasses.Polygon;
+
+public class RegularPolygon {
+	private int numberOfSides = 3;
+	private double sideLength = 1;
+	private double x = 0.0;
+	private double y = 0.0;
+	
+	public RegularPolygon() {
+		
+	}
+	double getPerimeter() {
+		return numberOfSides * sideLength;
+    }
+	double getArea() {
+		return numberOfSides * sideLength * sideLength / (4 * Math.tan (Math.PI / numberOfSides));
+    }
+}
+
---
+++ RegularPolygonTest.java
@@ -0,0 +1,16 @@
+package objectsAndClasses.Polygon;
+import java.util.Scanner;
+public class RegularPolygonTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		
+		
+		
+		input.close();
+
+	}
+
+}
