d703729795574eea2e6f6a87582e78e66251e548
Zlatozar Boev <66695667+StudentZ3@users.noreply.github.com>
2022-03-10 12:14:22 +0200

    Add files via upload

---
+++ Ball.java
@@ -0,0 +1,10 @@
+package week23_bouncyBallGame_MANYBalls_problem;
+import java.awt.Color;
+public class Ball {
+	int x = 0;
+	int y = 0; // Current ball position
+	int dx = 2; // Increment on ball's x-coordinate
+	int dy = 2; // Increment on ball's y-coordinate
+	int radius = 5; // Ball radius
+	Color color = new Color((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256));
+}
